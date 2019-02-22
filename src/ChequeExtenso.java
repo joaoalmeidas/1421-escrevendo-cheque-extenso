9c4065d6a5fc7382939f3b2ef1e1ce6e969ec9a1
joao <joao182cx@gmail.com>
2019-02-21 22:08:32 -0300

    Criação do código que adiciona o valor da parte das centenas por extenso na String valorExtenso

--- src/ChequeExtenso.java
+++ src/ChequeExtenso.java
@@ -17,8 +17,52 @@ public class ChequeExtenso {
 			System.out.println("Insira um valor menor do que 1000.00:");
 			valor = input.nextDouble();
 			
-		}while(valor <= 0 || valor > 1000.0);
-
+		}while(valor <= 0 || valor >= 1000.0);
+		
+		
+		if(valor > 99.0) {
+			
+			if(valor > 899.0) {
+				
+				valorExtenso += "NINE";
+				
+			}else if(valor > 799.0) {
+				
+				valorExtenso += "EIGHT";
+				
+			}else if(valor > 699.0) {
+				
+				valorExtenso += "SEVEN";
+				
+			}else if(valor > 599.0) {
+				
+				valorExtenso += "SIX";
+				
+			}else if(valor > 499.0) {
+				
+				valorExtenso += "FIVE";
+				
+			}else if(valor > 399.0) {
+				
+				valorExtenso += "FOUR";
+				
+			}else if(valor > 299.0) {
+				
+				valorExtenso += "THREE";
+				
+			}else if(valor > 199.0) {
+				
+				valorExtenso += "TWO";
+				
+			}else if(valor > 99.0) {
+				
+				valorExtenso += "ONE";
+				
+			}
+			
+			valorExtenso = " hundred";
+			
+		}
 		
 		
 	}
